92e5d4ddabf6aecdb01f6b9983b905cf7b43e7e4
eriko-creator <koenuma.eri@gmail.com>
2021-02-15 17:02:57 +0900

    MapSettingsのコードをstaticに変更、UI調整のため緯度を調節、不要なコードを削除

--- DriveSimulator/Model/Common/MapSettings.swift
+++ DriveSimulator/Model/Common/MapSettings.swift
@@ -11,30 +11,15 @@ import GooglePlaces
 
 class MapSettings{
     
-    //ピンを立てる
-    func addMarker (lat: Double, lng: Double, mapView:GMSMapView){
-    
-        let marker = GMSMarker()
-        //他のピンを消す
-        mapView.clear()
-        //ピンを立てる
-        marker.position = CLLocationCoordinate2D(latitude: lat, longitude: lng)
-        marker.map = mapView
-        marker.appearAnimation = .pop
-        //ピンを中心にしてカメラを動かす
-        updateCameraView(lat: lat, lng: lng, mapView: mapView, zoom: 20)
-        
-    }
-    
     //表示したい地点をviewの中央に持ってくる
-    func updateCameraView(lat: Double, lng: Double, mapView: GMSMapView, zoom:Int){
-        let center = GMSCameraPosition.camera(withLatitude: lat, longitude: lng, zoom: Float(zoom))
+    static func updateCameraView(lat: Double, lng: Double, mapView: GMSMapView, zoom:Int){
+        let center = GMSCameraPosition.camera(withLatitude: lat+0.0001, longitude: lng, zoom: Float(zoom))
         mapView.camera = center
         mapView.animate(to: center)
     }
     
     //位置情報を取得する
-    func requestLocation(){
+    static func requestLocation(){
         
         let locationManager = CLLocationManager()
         locationManager.requestWhenInUseAuthorization()
